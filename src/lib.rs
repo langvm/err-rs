23c208f5b1657ba7ae6aa376bbe8594e443a5349
Jelly Terra <jellyterra@proton.me>
2024-03-30 14:55:06 +0000

    Implement err! and wrap_result!

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+// Copyright 2024 LangVM Project
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0
+// that can be found in the LICENSE file and https://mozilla.org/MPL/2.0/.
+
+#[macro_export]
+macro_rules! err {
+    ($tag:expr, $expr:expr) => {
+        return Err($tag($expr));
+    };
+}
+
+#[macro_export]
+macro_rules! wrap_result {
+    ($tag:expr, $expr:expr) => {
+        match $expr {
+            Ok(v) => { Ok(v) }
+            Err(e) => { return Err($tag(e)); }
+        }?
+    };
+}
